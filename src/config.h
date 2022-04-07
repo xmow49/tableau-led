62ecb22ef52f3cf421e7a0528d9b6e5804487fe0
Xmow49 <dorian.benech007@gmail.com>
2022-04-07 20:00:11 +0100

    nettoyage

---
+++ src/config.h
@@ -0,0 +1,16 @@
+#define DEBUG false
+
+#define MAX_DISTANCE 200 // cm
+
+
+#define EVENT_SIZE (sizeof(struct inotify_event))
+#define BUF_LEN (1024 * (EVENT_SIZE + 16))
+
+#define IPC_PATH "/tmp/matrix"
+#define IPC_FILE "IPC"
+#define IPC_ALL IPC_PATH "/" IPC_FILE
+
+int SensorMesures[3] = {0, 0, 0};
+unsigned int currentSensor[3] = {0, 0, 0};
+unsigned int smoothSensorsValues[3] = {100, 100, 100};
+const unsigned char sensorsPins[3] = {8, 9, 25}; // https://fr.pinout.xyz/pinout/wiringpi //25 8 9
--- utils/time.sh
+++
@@ -1,1 +0,0 @@
-sudo timedatectl set-time "2022-04-05 16:25:00"
